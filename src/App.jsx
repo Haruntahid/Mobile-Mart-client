34be18e1130c78ceaceb3d3da977fbdf1956f768
haruntahid <tahidtaha997@gmail.com>
2024-08-15 04:14:49 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-6xl">Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
